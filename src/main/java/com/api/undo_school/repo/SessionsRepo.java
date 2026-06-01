5555ce163e4e0df1e6bb3c1c7eea33acb7aa8d62
nishithsai00 <137426824+nishithsai00@users.noreply.github.com>
2026-06-01 23:15:27 +0530

    Refactor method to use userId in findByTeacherId

--- src/main/java/com/api/undo_school/repo/SessionsRepo.java
+++ src/main/java/com/api/undo_school/repo/SessionsRepo.java
@@ -15,7 +15,7 @@ public interface SessionsRepo extends JpaRepository<Sessions,Integer> {
     @Query("SELECT s FROM Sessions s JOIN s.offering o WHERE s.teacherId.userId = :teacherId AND s.startTime < :endTime AND s.endTime > :startTime")
     List<Sessions> findOverLappingSession(@Param("teacherId") int teacherId,@Param("startTime") Instant startTime,@Param("endTime") Instant endTime);
 
-    List<Sessions> findByTeacherIdOrderByStartTimeAsc(int id);
+    List<Sessions> findByTeacherId_userIdOrderByStartTimeAsc(int id);
 
     @Query("SELECT s FROM Sessions s WHERE s.teacherId.userId = :teacherId AND s.startTime > :now AND s.offering.status != com.api.undo_school.model.Status.cancelled ORDER BY s.startTime ASC")
     List<Sessions> findUpcomingSessionsByTeacher(@Param("teacherId") int teacherId,@Param("now") Instant now);
